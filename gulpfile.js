6717626f14a2b626868b5a4ebc5661511baec173
m4rk3r <mbeasl1@gmail.com>
2016-01-02 12:33:12 -0500

    Foundation, added gulp, other dev tools

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+
+var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var source = require('vinyl-source-stream');
+
+gulp.task('sass', function (){
+    return gulp.src("scss/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("static/css"))
+    .pipe(browserSync.stream());
+});
+
+
+gulp.task('watch', function (){
+    browserSync.init({
+        server: {
+            baseDir: 'static'
+        }
+    });
+
+    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch(['static/index.html', 'static/js/main.js'], browserSync.reload);
+});
+
+gulp.task('default', ['watch']);
